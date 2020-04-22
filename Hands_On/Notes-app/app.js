d4a9016fd13de338ae4b4474de36eebbfa4a630a
nisargatmobi <nturke@mobiquityinc.com>
2020-04-22 10:40:29 +0530

    importing npm modules

--- Hands_On/Notes-app/app.js
+++ Hands_On/Notes-app/app.js
@@ -1,3 +1,5 @@
+const validator = require('validator')
 const fs = require('fs')
 fs.writeFileSync('notes.txt','this is my first node file')
 fs.appendFileSync('notes.txt',' this is appended message')
+console.log(validator.isEmail('nisarg@gmail.com'))
