567c7835d76825c5ad7d24c999c2d07397decce2
nisargatmobi <nturke@mobiquityinc.com>
2020-04-22 17:21:25 +0530

    minor chnages

--- Hands_On/playground/#1_Storing_and_retriving_json_data_from_file.js
+++ Hands_On/playground/#1_Storing_and_retriving_json_data_from_file.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
+
+// data from file is in binary form
 const dataBuffer = fs.readFileSync('jasonFile.json')
+// converted data to string
 const dataJSON = dataBuffer.toString()
+// no access field of object, string is converted to object
 const JSONObj = JSON.parse(dataJSON)
+// overided the field
 JSONObj.name = 'NISARG'
+// to store in file object is again coverted to string
 const overRidedString = JSON.stringify(JSONObj)
+// data is written to file
 fs.writeFileSync('jasonFile.json',overRidedString)
