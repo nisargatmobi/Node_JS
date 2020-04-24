aad5b1bb42fc128357d72d4d4b19c3b17854dd9b
nisargatmobi <nturke@mobiquityinc.com>
2020-04-24 17:47:05 +0530

    section 6 : http request challenge geoLocation

--- Hands_On/Weather-app/2. making http request.js
+++ Hands_On/Weather-app/2. making http request.js
@@ -4,3 +4,14 @@ request({ url: url},(error,response)=>{
     const data = JSON.parse(response.body)
     console.log("current temp is " + data.current.temperature + " current pressure is " + data.current.pressure)
 })
+
+
+const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibmlzYXJndHVya2UiLCJhIjoiY2s5ZTRua2FxMDF0ZTNsbXBuYXFmdW82dyJ9.s7SkV5CDHbA4BMFB12tCpw&limit=1'
+
+
+request({ url: geoCodeUrl, json: true },(error,response)=>{
+    const latitude = response.body.features[0].center[1]
+    const langitude = response.body.features[0].center[0]
+    console.log("latitude is : "+latitude," and langitude is : "+langitude)
+    
+})
